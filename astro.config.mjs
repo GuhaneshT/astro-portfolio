f1240eb950417e04da1eb8be9a5d7d0921e82800
Guhanesh22015 <cb.en.u4cse22015@cb.students.amrita.edu>
2025-06-23 00:03:50 +0530

    mmhm

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 
+
 export default defineConfig({
-  output: 'static',
-  base: '/Astro_Webpage/', // very important!
+  site: 'https://GuhaneshT.github.io/Astro_Webpage', // replace with your actual repo URL
 });
