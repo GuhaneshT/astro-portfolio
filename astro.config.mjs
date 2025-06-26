96439e9f4084656c09dbbde9aef80bb961528182
Guhanesh22015 <cb.en.u4cse22015@cb.students.amrita.edu>
2025-06-26 22:55:29 +0530

    added site

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  site: 'https://astro-portfolio-nnstdvwpe-guhaneshts-projects.vercel.app',
   integrations: [mdx(), sitemap(), tailwind()],
   // ❌ Remove or comment this line when deploying to Vercel:
   // site: 'https://GuhaneshT.github.io/Astro_Webpage',
