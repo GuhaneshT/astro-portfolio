9bb86a554d07d1b5745a8b74d01b35fbf4d2774c
Guhanesh22015 <cb.en.u4cse22015@cb.students.amrita.edu>
2025-06-26 22:50:12 +0530

    second downgrade change in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,10 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), tailwind()],
   // ❌ Remove or comment this line when deploying to Vercel:
   // site: 'https://GuhaneshT.github.io/Astro_Webpage',
 });
